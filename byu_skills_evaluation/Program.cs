88bdb1dcbc6ee2ded40ce262d84a8c3182bedac4
chess <chess@RICKYPC>
2016-09-21 14:50:30 -0600

    Add project files.

---
+++ byu_skills_evaluation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace byu_skills_evaluation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+
+            Console.ReadKey();
+        }
+    }
+}

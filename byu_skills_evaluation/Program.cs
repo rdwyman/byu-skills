3bbd7d14b00e4fd091396544b2cd6e2328c38a36
Ricky Wyman <chessofnerd@gmail.com>
2016-09-21 15:00:18 -0600

    Setting up remote repository

--- byu_skills_evaluation/Program.cs
+++ byu_skills_evaluation/Program.cs
@@ -11,7 +11,7 @@ namespace byu_skills_evaluation
         static void Main(string[] args)
         {
             Console.WriteLine("Hello!");
-
+            
             Console.ReadKey();
         }
     }

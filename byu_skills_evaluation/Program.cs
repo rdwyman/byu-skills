231e97ecfcc085f24463a15cd51bd4b4725e3ca6
Ricky Wyman <chessofnerd@gmail.com>
2016-09-23 00:19:01 -0600

    Added octokit.net submodule to help query github

--- byu_skills_evaluation/Program.cs
+++ byu_skills_evaluation/Program.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Net.Http;
+
+using System;
+using System.Net.Http;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace byu_skills_evaluation
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello!");
-
-            Console.WriteLine("Testing Remote!");
-            Console.ReadKey();
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+	}
 }

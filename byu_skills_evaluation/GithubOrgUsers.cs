a894c784d7066d8d62af21d3e8537dd81df85520
Ricky Wyman <chessofnerd@gmail.com>
2016-09-23 01:19:24 -0600

    First cut at Github organization query. Need to address proper credentialing.

---
+++ byu_skills_evaluation/GithubOrgUsers.cs
@@ -0,0 +1,32 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace byu_skills_evaluation
+{
+    internal class GithubOrgUsers
+    {
+        private string orgName;
+        IReadOnlyList<User> users;
+
+        public GithubOrgUsers(string orgName)
+        {
+            // set up a client
+            this.orgName = orgName;
+            GitHubClient ghClient = new GitHubClient(new ProductHeaderValue("Hello!"));
+            ghClient.Credentials = new Credentials("rdwyman", "FAKE");
+
+            // get the organization members
+            IOrganizationMembersClient omc = ghClient.Organization.Member;
+            users = omc.GetAll(this.orgName).Result;
+        }
+
+        public IEnumerable<User> FindMatchingUsers(Func<User,bool> testFunc)
+        {
+            return users.Where(testFunc);
+        }
+
+
+    }
+}
--- byu_skills_evaluation/Program.cs
+++ byu_skills_evaluation/Program.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Net.Http;
-
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-using System.Net;
+using Octokit;
 
 namespace byu_skills_evaluation
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string orgName = "BYUMicrostructureResearch";
+
+            Console.WriteLine("Starting");
+
+            // get all users of an organization whose name is null
+            GithubOrgUsers goc = new GithubOrgUsers(orgName);
+            IEnumerable<User> noNameUsers = goc.FindMatchingUsers(x => x.Name == null);
+            foreach (User user in noNameUsers)
+            {
+                Console.WriteLine(">>>" + user.Name + "|" + user.Login + "|" + user.Id + "<<<");
+            }
+
+            Console.WriteLine("Finished");
+            Console.ReadKey();
+
+        }
+    }
 }

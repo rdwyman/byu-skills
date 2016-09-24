7bcb14082d289472d5cb4eedf089c93bc0b29241
Ricky Wyman <chessofnerd@gmail.com>
2016-09-24 15:32:36 -0600

    Set up email. Added AWS submodule.

--- byu_skills_evaluation/Program.cs
+++ byu_skills_evaluation/Program.cs
@@ -19,9 +19,9 @@ namespace byu_skills_evaluation
             // set up data
             string orgName = "BYUMicrostructureResearch";
             //string orgName = "test-ricky-byu-skills";
-            Credentials githubCredentials = new Credentials("rdwyman", "FAKE");
+            Credentials githubCredentials = new Credentials("rdwyman", "FAKE_PASSWORD");
             string sftpEmail = "chessofnerd@gmail.com";
-            NetworkCredential sftpCredentials = new NetworkCredential("chessofnerd", "FAKE");
+            NetworkCredential sftpCredentials = new NetworkCredential("chessofnerd", "FAKE_PASSWORD");
             string valediction = "Yours,\nRicky Wyman";
 
             // get all users of an organization whose name is null
@@ -32,11 +32,11 @@ namespace byu_skills_evaluation
             // send email to users to tell them to add their name
             Console.WriteLine("Sending emails to users without name via \"" + sftpEmail + "\"");
             List<string> noNameUserEmails = noNameUsers.ConvertAll(x => x.Email);
-            List<string[]> noNameUserTokens = noNameUsers.ConvertAll(x => new string[] { x.Login, x.Email, x.Url });
+            List<string[]> noNameUserTokens = noNameUsers.ConvertAll(x => new string[] { x.Login, x.HtmlUrl });
             FormEmailSender fes = new FormEmailSender(sftpEmail, sftpCredentials);
             List<bool> emailSuccess = fes.SendFormEmail(
-                "Hi, %s! You are part of the Github organization " + orgName
-                + ". I noticed you haven't added your name to your Github profile. Please use the link %s"
+                "About your Github account","Hi, {0}! You are part of the Github organization " + orgName
+                + ". I noticed you haven't added your name to your Github profile. Please use the link {1}"
                 + " to update your profile!\n" + (valediction == null ? "Thanks!" : valediction)
                 , noNameUserEmails, noNameUserTokens);
 
--- byu_skills_evaluation/SendFormEmail.cs
+++ byu_skills_evaluation/SendFormEmail.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,18 +10,56 @@ namespace byu_skills_evaluation
 {
     class FormEmailSender
     {
-        private NetworkCredential sftpCredentials;
-        private string sftpEmail;
+        private readonly string sftpEmail;
+        private readonly SmtpClient client;
 
         public FormEmailSender(string sftpEmail, NetworkCredential sftpCredentials)
         {
             this.sftpEmail = sftpEmail;
-            this.sftpCredentials = sftpCredentials;
+
+            client = new SmtpClient();
+            client.Credentials = sftpCredentials;
+            client.Port = 587;
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+
         }
 
-        internal List<bool> SendFormEmail(string formEmail, List<string> emailAddresses, List<string[]> tokens)
+        internal List<bool> SendFormEmail(string subject, string formEmail, List<string> emailAddresses, List<string[]> tokens)
         {
-            throw new NotImplementedException();
+            // http://stackoverflow.com/questions/757987/send-email-via-c-sharp-through-google-apps-account answer by Achilles
+
+            for (int i = 0; i < emailAddresses.Count; i++)
+            {
+                try
+                {
+                    string curEmail = emailAddresses[i];
+                    string[] curTokenArray = tokens[i];
+                    if (curEmail != null)
+                    {
+                        MailAddress maFrom = new MailAddress(sftpEmail, "Sender's Name", Encoding.UTF8);
+                        MailAddress maTo = new MailAddress(curEmail, "Recipient's Name", Encoding.UTF8);
+                        MailMessage mmsg = new MailMessage(maFrom.Address, maTo.Address);
+                        mmsg.Body = string.Format(formEmail, tokens[i]);
+                        mmsg.BodyEncoding = Encoding.UTF8;
+                        mmsg.IsBodyHtml = true;
+                        mmsg.Subject = subject;
+                        mmsg.SubjectEncoding = Encoding.UTF8;
+
+                        client.Send(mmsg);
+                    }
+                    else
+                    {
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString(), ex.Message);
+                }
+            }
+
+            return null;
         }
     }
 }
